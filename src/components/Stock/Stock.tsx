0be90760c4034070022828aa913642e27abc4a00
andreidragos <andrei@inforitus.nl>
2020-04-22 17:01:15 +0300

    wip

--- src/components/Stock/Stock.tsx
+++ src/components/Stock/Stock.tsx
@@ -6,39 +6,39 @@ import React, { useState, useEffect } from 'react';
 import { SearchFormValues } from './stock.types';
 
 type Props = {
-	children: React.ReactNode
+    children: React.ReactNode
 };
 
 function Stock(props: Props) {
-	const chartData = useSelector((state: any) => state.stocks.chartData);
-	const isLoading = useSelector((state: any) => state.stocks.isLoading);
-	const dispatch = useDispatch();
-	
-	const initialSearchValues: SearchFormValues = {
-		company: 'DAX',
-		startDate: '2018-01-01',
-		endDate: '2019-02-01'
-	};
-	
-	useEffect(() => {
-		dispatch(stockActions.getData(initialSearchValues));
-	}, []);
+    const chartData = useSelector((state: any) => state.stocks.chartData);
+    const isLoading = useSelector((state: any) => state.stocks.isLoading);
+    const dispatch = useDispatch();
+    
+    const initialSearchValues: SearchFormValues = {
+        company: 'DAX',
+        startDate: '2018-01-01',
+        endDate: '2019-02-01'
+    };
+    
+    useEffect(() => {
+        dispatch(stockActions.getData(initialSearchValues));
+    }, []);
 
-	function onSearchSubmit(values: SearchFormValues): void {
-		event.preventDefault();
-		dispatch(stockActions.getData(values));
-	}
+    function onSearchSubmit(values: SearchFormValues): void {
+        event.preventDefault();
+        dispatch(stockActions.getData(values));
+    }
     
     return (
         <div>
-			<h1>Stock Market</h1>
-			<StockSearch
-				onSearchSubmit={onSearchSubmit}
-				initialValues={initialSearchValues} />
-			<StockChart
-				chartData={chartData}
-				isLoading={isLoading}
-			/>
+            <h1>Stock Market</h1>
+            <StockSearch
+                onSearchSubmit={onSearchSubmit}
+                initialValues={initialSearchValues} />
+            <StockChart
+                chartData={chartData}
+                isLoading={isLoading}
+            />
         </div>
     )
 }
